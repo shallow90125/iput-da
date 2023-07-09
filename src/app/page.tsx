3e8cad9932d6365854f05a42fdf8e29caa0c66f2
shallow90125 <shallow90125@gmail.com>
2023-07-09 13:27:25 +0900

    modify layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
+import HomeWidget from "@/components/HomeWidget";
+import { Flex, Grid, Stack, Text } from "@/components/chakra-ui/react";
+
 export default function Home() {
-  return <>home</>;
+  return (
+    <Flex
+      placeContent="streach"
+      placeItems="streach"
+      gap={4}
+      p={4}
+      direction="column"
+    >
+      <Stack flex="none">
+        <Text fontSize="3xl">3つの病気のリスクをAIが診断します</Text>
+      </Stack>
+      <Grid templateRows="repeat(3, 1fr)" gap="4" flexGrow={1}>
+        <HomeWidget
+          href="/diabetes"
+          text="糖尿病"
+          bgColor="pink.300"
+          describe="症状とライフスタイルを分析し、糖尿病の可能性を評価します"
+        ></HomeWidget>
+        <HomeWidget
+          href="/diabetes"
+          text="高血圧"
+          bgColor="cyan.300"
+          describe="血圧と生活習慣から、高血圧のリスクを予測します"
+        ></HomeWidget>
+        <HomeWidget
+          href="/diabetes"
+          text="脳卒中"
+          bgColor="yellow.300"
+          describe="異常な症状を検出し、脳卒中の早期警告を提供します"
+        ></HomeWidget>
+      </Grid>
+    </Flex>
+  );
 }
---
+++ src/components/HomeWidget.tsx
@@ -0,0 +1,41 @@
+import { Link } from "@/components/chakra-ui/next-js";
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  Text,
+} from "@/components/chakra-ui/react";
+
+type Props = {
+  href: string;
+  text: string;
+  bgColor: string;
+  describe: string;
+};
+
+export default function HomeWidget(props: Props) {
+  return (
+    <GridItem
+      display="flex"
+      placeContent="streach"
+      placeItems="streach"
+      bgColor={props.bgColor}
+      color="blackAlpha.900"
+      rounded="lg"
+      p="2"
+    >
+      <Box flex={1}>
+        <Text fontSize="2xl" fontWeight="bold">
+          {props.text}
+        </Text>
+        <Text>{props.describe}</Text>
+      </Box>
+      <Grid flex="none" placeItems="end">
+        <Link href={props.href}>
+          <Button bgColor="green.50">診断する</Button>
+        </Link>
+      </Grid>
+    </GridItem>
+  );
+}
